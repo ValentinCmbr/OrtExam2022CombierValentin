d5bc4ab3e8703b35c2f41b56996f2d005fdc1814
ValentinCmbr <v.combier@lyon.ort.asso.fr>
2022-06-14 16:46:04 +0200

    Step 4 | Navbar
    Affichage de la navbar

--- App.js
+++ App.js
@@ -12,20 +12,21 @@ import {
     ScrollView,
     SafeAreaViewComponent
 } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
 
-  const [liste, setListe] = useState(null)
-  const [number, onChangeNumber] = useState(null);
-  const [texte, onChangeTexte] = useState(null)
+    const [liste, setListe] = useState(null)
+    const [number, onChangeNumber] = useState(null);
+    const [texte, onChangeTexte] = useState(null)
 
-  const getSimpson = () => {
-      fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=1`)
-        .then(response => response.json())
-        .then(data => {
-          setListe(data);
-        })}
+    const getSimpson = () => {
+        fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=1`)
+            .then(response => response.json())
+            .then(data => {
+                setListe(data);
+            })}
 
     const reload = () => {
         fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${number}&character=${texte}`)
@@ -34,43 +35,72 @@ export default function App() {
                 setListe(data);
             })}
 
-  useEffect(() => {
-        (async () => {
-          getSimpson()
-        })();
-      },
-      []);
+    const random = () => {
+        fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?`)
+            .then(response => response.json())
+            .then(data => {
+                setListe(data);
+            })}
 
-  const Item = ({title}) => (
-    <View>
-      <Text style={styles.name}>{title.character}</Text>
-      <Text>{title.quote}</Text>
-       <Image style={styles.tinyLogo} source={{uri: title.image}}/>
-    </View>
-);
 
-  const renderItem = ({ item }) => (
-    <Item title={item} />
-  );
-  return (
-      <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <TextInput style={styles.input} onChangeText={onChangeNumber} value={number} placeholder="Choisir nombre" keyboardType="numeric"/>
-        <TextInput style={styles.input} onChangeText={onChangeTexte} value={texte} placeholder="Choisir un personnage" keyboardType="ascii-capable"></TextInput>
-          <TouchableOpacity style={styles.button} onPress={reload}><Text>Confirmer la recherche</Text></TouchableOpacity>
-      <FlatList data={liste} renderItem={renderItem}></FlatList>
-      </ScrollView>
-      </SafeAreaView>
-  );
+    const Item = ({title}) => (
+        <View>
+            <Text style={styles.name}>{title.character}</Text>
+            <Text>{title.quote}</Text>
+            <Image style={styles.tinyLogo} source={{uri: title.image}}/>
+        </View>
+    );
+
+    const renderItem = ({ item }) => (
+        <Item title={item} />
+    );
+
+    function HomeScreen() {
+        return (
+            <SafeAreaView style={styles.container}>
+                    <TouchableOpacity style={styles.button} onPress={random}><Text>random</Text></TouchableOpacity>
+                    <FlatList data={liste} renderItem={renderItem}></FlatList>
+            </SafeAreaView>
+        );
+    }
+
+    function SettingsScreen() {
+        return (
+            <SafeAreaView style={styles.container}>
+                    <TextInput style={styles.input} onChangeText={onChangeNumber} value={number} placeholder="Choisir nombre" keyboardType="numeric"/>
+                    <TextInput style={styles.input} onChangeText={onChangeTexte} value={texte} placeholder="Choisir un personnage" keyboardType="ascii-capable"></TextInput>
+                    <TouchableOpacity style={styles.button} onPress={reload}><Text>Confirmer la recherche</Text></TouchableOpacity>
+                    <FlatList data={liste} renderItem={renderItem}></FlatList>
+
+            </SafeAreaView>
+        );
+    }
+
+    const Tab = createBottomTabNavigator();
+
+    function MyTabs() {
+        return (
+            <Tab.Navigator>
+                <Tab.Screen name="Aleatoire" component={HomeScreen} />
+                <Tab.Screen name="Recherche" component={SettingsScreen} />
+            </Tab.Navigator>
+        );
+    }
+
+    return (
+        <NavigationContainer>
+            <MyTabs />
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     tinyLogo: {
         width: 120,
         height: 120,
@@ -92,7 +122,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
     },
     name: {
-      color:"#36b6d9",
+        color:"#36b6d9",
     }
 
 });
