f0c24457bcca730d3fa1e9b965ba7d3f5723e76c
ValentinCmbr <v.combier@lyon.ort.asso.fr>
2022-06-14 13:50:34 +0200

    First commit | STEP 1 sans bouton reload et sans réel mise en page
    First commit | STEP 1 sans bouton reload et sans réel mise en page.

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image, SafeAreaView, FlatList, TouchableOpacity} from 'react-native';
+import {useEffect, useState} from "react";
+
 
 export default function App() {
+
+  const [liste, setListe] = useState(null)
+  const [newState, setNewState] = useState(false);
+
+  const getSimpson = () => {
+    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+        .then(response => response.json())
+        .then(data => {
+          setListe(data);
+        })}
+
+    const reload = () => React.useCallback(() => {
+        setNewState(true);
+        wait(2000).then(() => setNewState(false));
+    }, []);
+
+  useEffect(() => {
+        (async () => {
+          getSimpson()
+        })();
+      },
+      []);
+
+  const Item = ({title}) => (
+    <View>
+      <Text>{title.character}</Text>
+      <Text>{title.quote}</Text>
+        <Image style={styles.tinyLogo} source={{uri: title.image}}/>
+    </View>
+);
+
+  const renderItem = ({ item }) => (
+    <Item title={item} />
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList data={liste} renderItem={renderItem}></FlatList>
     </View>
   );
 }
@@ -17,4 +52,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    tinyLogo: {
+        width: 120,
+        height: 120,
+        borderRadius:4,
+    },
 });
